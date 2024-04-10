3457e4d29a58c8a5dc2daf21bfe7616717336fbf
MashroomMan <thecheesemaker@protonmail.ch>
2024-04-10 16:50:20 -0400

    Added pass table amongst others :/

--- create_db.py
+++ create_db.py
@@ -84,8 +84,8 @@ def setupCompetitions():
         country_name VARCHAR(255),
         competition_name VARCHAR(255),
         competition_gender VARCHAR(50),
-        competition_youth BOOLEAN,
-        competition_international BOOLEAN,
+        competition_youth BITEAN,
+        competition_international BITEAN,
         season_name VARCHAR(255),
         PRIMARY KEY (competition_id, season_id)
     );
@@ -164,13 +164,41 @@ def setupEvents():
         PRIMARY KEY (id)
     );
 
-
-    CREATE TABLE IF NOT EXISTS pass (
+    CREATE TABLE IF NOT EXISTS passHeight (
         id INTEGER NOT NULL,
         name TEXT,
         PRIMARY KEY (id)
     );
 
+    
+    CREATE TABLE IF NOT EXISTS bodyPart (
+        id INTEGER NOT NULL,
+        name TEXT,
+        PRIMARY KEY (id)
+    );
+
+
+    CREATE TABLE IF NOT EXISTS technique (
+        id INTEGER NOT NULL,
+        name TEXT,
+        PRIMARY KEY (id)
+    );
+
+    
+    CREATE TABLE IF NOT EXISTS skillType (
+        id INTEGER NOT NULL,
+        name TEXT,
+        PRIMARY KEY (id)
+    );
+
+    
+    CREATE TABLE IF NOT EXISTS outcome (
+        id INTEGER NOT NULL,
+        name TEXT,
+        PRIMARY KEY (id)
+    );
+
+    
     CREATE TABLE IF NOT EXISTS events (
         id UUID,
         index INTEGER NOT NULL,
@@ -186,7 +214,6 @@ def setupEvents():
         duration FLOAT,
         location FLOAT[],
         carry_end_location FLOAT[],
-        pass
 
         PRIMARY KEY (id),
 
@@ -194,6 +221,44 @@ def setupEvents():
         FOREIGN KEY (possession_team) REFERENCES eventTeams(id),
         FOREIGN KEY (play_pattern) REFERENCES eventPlayPattern(id),
         FOREIGN KEY (team) REFERENCES eventTeams(id)
+    );
+    
+
+    CREATE TABLE IF NOT EXISTS pass (
+        recipientId INTEGER NOT NULL,
+        length FLOAT NOT NULL,
+        angle FLOAT NOT NULL,
+        heightId INTEGER NOT NULL,
+        end_location FLOAT[] NOT NULL,
+        bodyPartId INTEGER NOT NULL,
+        outcomeId INTEGER,
+        assisted_shot_id INTEGER,
+        miscommunication BIT,
+        crossed BIT,
+        cut_back BIT,
+        shot_assist BIT,
+        goal_assist BIT,
+        inswinging BIT,
+        outswinging BIT,
+        switch BIT,
+        through_ball BIT,
+        no_touch BIT,
+        straight BIT,
+        aerial_won BIT,
+        deflected BIT,
+        techniqueId INTEGER,
+        skillTypeId INTEGER,
+        eventId UUID NOT NULL,
+
+        PRIMARY KEY (recipientId, length, angle, heightId),
+
+        FOREIGN KEY (recipientId) REFERENCES player(id),
+        FOREIGN KEY (heightId) REFERENCES passHeight(id),
+        FOREIGN KEY (bodyPartId) REFERENCES bodyPart(id),
+        FOREIGN KEY (techniqueId) REFERENCES technique(id),
+        FOREIGN KEY (skillTypeId) REFERENCES skillType(id),
+        FOREIGN KEY (outcomeId) REFERENCES outcome(id),
+        FOREIGN KEY (eventId) REFERENCES events(id)
     );
 
 
@@ -270,59 +335,86 @@ def setupEvents():
 
         print ("Iterating through files now")
 
-        for file in os.listdir(path_to_data):
-            if file in relevantEventFiles:
-                if file.endswith(".json"):
-                    file_path = os.path.join(path_to_data, file)
-                    with open(file_path, 'r') as file:
-                        events_table_data = json.load(file)
+        # for file in os.listdir(path_to_data):
+        #     if file in relevantEventFiles:
+        #         if file.endswith(".json"):
+        #             file_path = os.path.join(path_to_data, file)
+        #             with open(file_path, 'r') as file:
+        #                 events_table_data = json.load(file)
 
-                        for entity in events_table_data:
-                            cursor.execute(eventType_table_dml, (
-                            entity['type']['id'],
-                            entity['type']['name']))
+        #                 for entity in events_table_data:
+        #                     cursor.execute(eventType_table_dml, (
+        #                     entity['type']['id'],
+        #                     entity['type']['name']))
 
-                            cursor.execute(eventPlayPattern_table_dml, (
-                            entity['play_pattern']['id'],
-                            entity['play_pattern']['name']))
+        #                     cursor.execute(eventPlayPattern_table_dml, (
+        #                     entity['play_pattern']['id'],
+        #                     entity['play_pattern']['name']))
 
-                            cursor.execute(eventTeams_table_dml, (
-                            entity['team']['id'],
-                            entity['team']['name']))
+        #                     cursor.execute(eventTeams_table_dml, (
+        #                     entity['team']['id'],
+        #                     entity['team']['name']))
 
-                            if 'tactics' in entity:
-                                for lineup_entity in (entity['tactics']['lineup']):
+        #                     if 'tactics' in entity:
+        #                         for lineup_entity in (entity['tactics']['lineup']):
                                 
-                                    cursor.execute(player_table_dml, (
-                                    lineup_entity['player']['id'],
-                                    lineup_entity['player']['name']))
+        #                             cursor.execute(player_table_dml, (
+        #                             lineup_entity['player']['id'],
+        #                             lineup_entity['player']['name']))
 
-                                    cursor.execute(position_table_dml, (
-                                    lineup_entity['position']['id'],
-                                    lineup_entity['position']['name']))
+        #                             cursor.execute(position_table_dml, (
+        #                             lineup_entity['position']['id'],
+        #                             lineup_entity['position']['name']))
                             
-                            cursor.execute(events_table_dml, (
-                            entity['id'],
-                            entity['index'],
-                            entity['period'],
-                            entity['timestamp'],
-                            entity['minute'],
-                            entity['second'],
-                            entity['type']['id'],
-                            entity['possession'],
-                            entity['possession_team']['id'],
-                            entity['play_pattern']['id'],
-                            entity['team']['id'],
-                            entity['duration']))
+        #                     cursor.execute(events_table_dml, (
+        #                     entity['id'],
+        #                     entity['index'],
+        #                     entity['period'],
+        #                     entity['timestamp'],
+        #                     entity['minute'],
+        #                     entity['second'],
+        #                     entity['type']['id'],
+        #                     entity['possession'],
+        #                     entity['possession_team']['id'],
+        #                     entity['play_pattern']['id'],
+        #                     entity['team']['id'],
+        #                     entity['duration']))
                             
 
 
         conn.commit()
 
+
+def extract_attributes():
+
+    path_to_data = "/home/cheesemaker/Documents/COMP3005/team_project/open-data-master/data/events"
+
+    relevantEventFiles = getRelevantFiles()
+
+    print ("Iterating through files now")
+
+    pass_attr = set()
+
+
+    for file in os.listdir(path_to_data):
+        if file in relevantEventFiles:
+            if file.endswith(".json"):
+                file_path = os.path.join(path_to_data, file)
+                with open(file_path, 'r') as file:
+                    events_table_data = json.load(file)
+
+                    for entity in events_table_data:
+
+                        if 'pass' in entity:
+                            pass_attr.update(entity['pass'].keys())
+
+    print(pass_attr)
+
 def main():
 
-    setupCompetitions()
+    # setupCompetitions()
     setupEvents()
+    # extract_attributes()
     conn.close()
 
 if __name__ == "__main__":
