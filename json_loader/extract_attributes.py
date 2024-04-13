6e71c318fd5330b67963a2c68f5ebb2270080ed7
Aparna Apu <apu.apru@gmail.com>
2024-04-12 22:02:02 -0400

    Correcting folder structure

--- json_loader/extract_attributes.py
+++ json_loader/extract_attributes.py
@@ -58,11 +58,13 @@ def getRelevantFiles():
 # Get all attributes for a type of file
 ###############################################################################################
 
-def extract_attributes():
+def extract_attributes_for_events():
 
     path_to_data = "/home/student/Documents/comp3005/project_v1/open-data/data/events"
     
     relevant_event_files = getRelevantFiles()
+
+    print(relevant_event_files)
 
     print("Iterating through the files ...")
 
@@ -113,5 +115,67 @@ def extract_attributes():
         else:
             print(attribute)
 
-# Call the functions
-extract_attributes()
+###############################################################################################
+# Get all attributes for a type of file (but matches now)
+###############################################################################################
+
+def extract_attributes_for_matches():
+
+    path_to_data = "/home/student/Documents/comp3005/project_v1/open-data/data/matches"
+
+    # Seasons
+    # 2018/2019, 2019/2020, 2020/2021, 2003/2004
+    relevant_seasons = ["44.json", "4.json", "42.json", "90.json"]
+    
+    # Leagues
+    # La Liga and Premier League
+    relevant_comp_id = ["2", "11"]
+     
+    attributes = []
+
+    # Recursive helper
+    def add_keys_from_dict(data_dict, prefix = ''):
+        for key, value in data_dict.items():
+            if (prefix != ''):
+                path = prefix + '.' + key
+            else:
+                path = key
+
+            # Prevent dupliactes from being added
+            if path not in attributes:
+                attributes.append(path)
+
+            # Check if it is nested/dictionary
+            # If it is, then there is more than just one simple attribute
+            # So recursively call the function to add the innards too
+            if isinstance(value, dict):
+                add_keys_from_dict(value, path)
+            # Check is dictionary
+            # Make sure there is a value
+            # And then also make sure that the first element is a dictionary so that its not []
+            elif isinstance(value, list) and value and isinstance(value[0], dict):
+                add_keys_from_dict(value[0], path)
+
+    for folder in os.listdir(path_to_data):
+        # Filter on folder (PL, LL)
+        if (folder in relevant_comp_id):
+            folder_path = os.path.join(path_to_data, folder)
+
+            for file in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, file)
+                # Filter on file 
+                if (file in relevant_seasons):
+                    with open(file_path, 'r') as f:
+                        matches_table_data = json.load(f)
+
+                        for match in matches_table_data:
+                            add_keys_from_dict(match)
+
+    for attribute in attributes:
+        if ("." in attribute):
+            print("   " + attribute)
+        else:
+            print(attribute)
+
+# Call helper here
+extract_attributes_for_matches()
